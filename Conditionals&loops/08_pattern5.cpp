58d353467459e149c570e7f772a7adfe2568d135
Shubham <shubham7703@gmail.com>
2020-10-07 15:18:19 +0530

    added pattern codes

---
+++ Conditionals&loops/08_pattern5.cpp
@@ -0,0 +1,37 @@
+/* pattern 5
+    n=4;
+
+       1
+      23
+     345
+    4567
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int i=1;
+    while(i<=n){
+        int count=i;
+
+        int j=1;
+        while(j<=n-i){
+            cout<<" ";
+            j++;
+        }
+
+        j=1;
+        while(j<=i){
+            cout<<count;
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    
+}
